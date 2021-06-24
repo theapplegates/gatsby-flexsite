f7a6d134452260a8cc9b8a5c1fe658f0454df87c
Paul <paul@theapplegates.com>
2021-06-24 18:34:45 -0400

    Update sidebar.js

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,70 +4,81 @@ import React from 'react'
 import Logo from './pic.jpg'
 
 const Sidebar = ({ siteMetadata }) => (
-  <>
-    <aside className="sidebar">
-      <header>
-      <script src="https://kit.fontawesome.com/1fca07cddd.js" crossorigin="anonymous"></script>
-        <div className="about">
-          <div className="cover-author-image">
-            <Link to="/">
-              <img src={Logo} alt={siteMetadata.author} />
-            </Link>
-          </div>
-          <div className="author-name">{siteMetadata.author}</div>
-          <p>{siteMetadata.description}</p>
-        </div>
-      </header>
-      <footer>
-        <section className="contact">
-          <h3 className="contact-title">Contact me</h3>
-          <ul>
-        {siteMetadata.social.facebook && (
-              <li>
-                <a
-                  href={`https://facebook.com/${siteMetadata.social.facebook}`}
-                  target="_blank">
-                  <i class="fa-thin fa-shield-check"></i>
-                </a>
-              </li>
-            )}
-            {siteMetadata.social.github && (
-              <li>
-                <a
-                  href={`https://github.com/${siteMetadata.social.github}`}
-                  target="_blank"
-                >
-                  <i className="fa fa-github" aria-hidden="true" />
-                </a>
-              </li>
-            )}
-            {siteMetadata.social.linkedin && (
-              <li>
-                <a
-                  href={`https://linkedin.com/in/${siteMetadata.social.linkedin}`}
-                  target="_blank"
-                >
-                  <i className="fa fa-linkedin" aria-hidden="true" />
-                </a>
-              </li>
-            )}
-            {siteMetadata.social.email && (
-              <li>
-                <a href={`mailto:${siteMetadata.social.email}`} target="_blank">
-                  <i className="fa fa-envelope-o" aria-hidden="true" />
-                </a>
-              </li>
-            )}
-          </ul>
-        </section>
-        <div className="copyright">
-          <p>
-            {new Date().getFullYear()} &copy; {siteMetadata.author}
-          </p>
-        </div>
-      </footer>
-    </aside>
-  </>
+	<>
+		<aside className="sidebar">
+			<header>
+			<script src="https://kit.fontawesome.com/1fca07cddd.js" crossorigin="anonymous"></script>
+				<div className="about">
+					<div className="cover-author-image">
+						<Link to="/">
+							<img src={Logo} alt={siteMetadata.author} />
+						</Link>
+					</div>
+					<div className="author-name">{siteMetadata.author}</div>
+					<p>{siteMetadata.description}</p>
+				</div>
+			</header>
+			<footer>
+				<section className="contact">
+					<h3 className="contact-title">Contact me</h3>
+					<ul>
+						{siteMetadata.social.twitter && (
+							<li>
+								<a
+									href={`https://twitter.com/${siteMetadata.social.twitter}`}
+									target="_blank"
+								>
+									<i className="fa fa-twitter" aria-hidden="true" />
+								</a>
+							</li>
+						)}
+						{siteMetadata.social.facebook && (
+							<li>
+								<a
+									href={`https://facebook.com/${siteMetadata.social.facebook}`}
+									target="_blank"
+								>
+									<i className="fa fa-facebook" aria-hidden="true" />
+								</a>
+							</li>
+						)}
+						{siteMetadata.social.github && (
+							<li>
+								<a
+									href={`https://github.com/${siteMetadata.social.github}`}
+									target="_blank"
+								>
+									<i className="fa fa-github" aria-hidden="true" />
+								</a>
+							</li>
+						)}
+						{siteMetadata.social.linkedin && (
+							<li>
+								<a
+									href={`https://linkedin.com/in/${siteMetadata.social.linkedin}`}
+									target="_blank"
+								>
+									<i className="fa fa-linkedin" aria-hidden="true" />
+								</a>
+							</li>
+						)}
+						{siteMetadata.social.email && (
+							<li>
+								<a href={`mailto:${siteMetadata.social.email}`} target="_blank">
+									<i className="fa fa-envelope-o" aria-hidden="true" />
+								</a>
+							</li>
+						)}
+					</ul>
+				</section>
+				<div className="copyright">
+					<p>
+						{new Date().getFullYear()} &copy; {siteMetadata.author}
+					</p>
+				</div>
+			</footer>
+		</aside>
+	</>
 )
 
 export default Sidebar
